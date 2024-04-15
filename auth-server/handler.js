176953c2cda543b432738525bad5871692ba1125
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-04-15 09:18:38 -0400

    update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,7 +20,7 @@ const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly
 
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://codestun.github.io/event-explorer/"
+  "https://mikeb-molina.github.io/meet/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
