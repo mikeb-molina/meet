15293f5bb96ca29718614e544cc2602071774f9d
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-04-15 08:49:18 -0400

    add getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { response } = require("express");
 const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
@@ -66,13 +67,56 @@ module.exports.getAccessToken = async (event) => {
       };
     })
     .catch((error) => {
-      // Handle error
+      console.log(error)
       return {
         statusCode: 500,
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials': true,
         },
+        body: JSON.stringify(error),
+      };
+    });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+      calendarId: CALENDAR_ID,
+      auth: oAuth2Client,
+      timeMin: new Date().toISOString(),
+      singleEvents: true,
+      orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject (error);
+        } else {
+          resolve (response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) =>{
+      return{
+        statusCode: 500,
         body: JSON.stringify(error),
       };
     });
